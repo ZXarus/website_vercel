79d79b91a92e27dc7fd0590a2726f6dd72dfc97f
Geetanjali <geetanjali9361@gmail.com>
2025-08-20 17:13:27 +0530

    Added automatic entry to the user_has_roles table for the role based access

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const user = require('../controller/user');
+const createProperty = require('../controller/createProperty');
 
+router.post('/property-by-user',createProperty.propertyByUser)
 
 module.exports = router;
